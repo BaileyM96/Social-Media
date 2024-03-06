9a897f366eb1d5409225e60e59a63b0ae3a55e50
Bailey Mejia <baileymejia28@gmail.com>
2024-03-05 21:05:05 -0800

    Created Login page with two input fields to login with email and password

---
+++ socialmedia/src/app/login/login.styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import TextField  from "@mui/material/TextField";
+import Button  from "@mui/material/Button";
+
+export const LoginContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+`;
+
+export const StyledLoginField = styled(TextField)`
+padding: 12px;
+`;
+
+export const StyledLoginButton = styled(Button)`
+
+`;
---
+++ socialmedia/src/app/login/page.js
@@ -0,0 +1,35 @@
+'use client';
+import React from "react";
+import { useState } from "react";
+import { useMutation, gql } from "@apollo/client";
+import { StyledHeader } from "../signup/signup.styled";
+import { LoginContainer, StyledLoginField, StyledLoginButton } from "./login.styled";
+import Link from 'next/link'
+
+export default function Login() {
+    return (
+        <>
+        <StyledHeader>Login</StyledHeader>
+
+        <LoginContainer>
+            <StyledLoginField 
+                required
+                id="outlined-basic"
+                label="Email"
+                name="email" 
+                variant="outlined"
+            />
+            <StyledLoginField 
+                required
+                id="outlined-basic"
+                label="Password"
+                name="Password"
+                type="password" 
+                variant="outlined"
+            />
+            <StyledLoginButton variant="outlined">Login</StyledLoginButton>
+            <Link href='/signup'>Dont have an account? Signup here!</Link>
+        </LoginContainer>
+        </>
+    )
+}
