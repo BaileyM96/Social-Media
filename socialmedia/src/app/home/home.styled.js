c12723c433cd88222df933b2418f6f19b2dd17cd
Bailey Mejia <baileymejia28@gmail.com>
2024-03-10 20:45:55 -0700

    Started creating the design for the homepage and writing tests for the posts

---
+++ socialmedia/src/app/home/home.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@mui/material/CardContent";
+
+export const StyledHomeContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+`;
+
+export const StyledCard = styled(Card)`
+
+`;
--- socialmedia/src/app/home/page.js
+++ socialmedia/src/app/home/page.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { StyledHomeContainer, StyledCard } from "./home.styled";
 
 export default function Home() {
     return (
-        <div>
-            <h1>Welcome to the home page</h1>
-        </div>
+        <>
+            <div>
+                <h1>Welcome to the home page</h1>
+            </div>
+
+            <StyledHomeContainer>
+                <StyledCard>
+                    <CardContent>
+                        <h3>Post content from friends</h3>
+                    </CardContent>
+                </StyledCard>
+            </StyledHomeContainer>
+        </>
     )
 };
