c68c6fa30789d0b38985bc0587084a6b6c7934d4
Bailey <baileymejia28@gmail.com>
2024-03-12 17:25:12 -0700

    Created the basic design for the posts in the homepage

--- socialmedia/src/app/home/home.styled.js
+++ socialmedia/src/app/home/home.styled.js
@@ -3,16 +3,19 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
+import Avatar from "@mui/material/Avatar";
 
 export const StyledHomeContainer = styled.div`
 display: flex;
 justify-content: center;
 align-items: center;
 flex-direction: column;
+row-gap: 10px;
+padding: 2%;
 `;
 
 export const StyledCard = styled(Card)`
-
+/* padding-top: 12px !important */
 `;
 
 export const StyledCardContent = styled(CardContent)`
@@ -20,5 +23,13 @@ export const StyledCardContent = styled(CardContent)`
 `;
 
 export const StyledCardHeader = styled(CardHeader)`
+padding-bottom: 5px !important;
+`;
+
+export const StyledAvatar = styled(Avatar)`
+
+`;
+
+export const StyledCardActions = styled(CardActions)`
 
 `;
--- socialmedia/src/app/home/page.js
+++ socialmedia/src/app/home/page.js
@@ -1,21 +1,52 @@
 'use client';
 import React from "react";
-import { StyledHomeContainer, StyledCard, StyledCardContent } from "./home.styled";
+import { StyledHomeContainer, StyledCard, StyledCardContent, StyledAvatar, StyledCardHeader, StyledCardActions } from "./home.styled";
+import IconButton  from "@mui/material/IconButton";
+import Favorite from "@mui/icons-material/Favorite";
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+
 
 export default function Home() {
     return (
         <>
-            <div>
-                <h1>Welcome to the home page</h1>
-            </div>
 
             <StyledHomeContainer>
                 <StyledCard>
+                    <StyledCardHeader 
+                    title="username" 
+                    subheader="9-11-24"
+                    avatar={<StyledAvatar></StyledAvatar>}
+                    >
+                    </StyledCardHeader>
                     <StyledCardContent>
-                        <h3>Post content from friends</h3>
+                        This is my post and lets see how it looks This is my post and lets see how it looks This is my post and lets see how it looks
                     </StyledCardContent>
+                    <StyledCardActions>
+                        <IconButton aria-label="add to favorites">
+                            <Favorite /> 100
+                        </IconButton>
+                    </StyledCardActions>
+                </StyledCard>
+    
+                <StyledCard>
+                    <StyledCardHeader 
+                    title="username" 
+                    subheader="9-11-24"
+                    avatar={<StyledAvatar></StyledAvatar>}
+                    >
+                    </StyledCardHeader>
+                    <StyledCardContent>
+                        This is my post and lets see how it looks This is my post and lets see how it looks This is my post and lets see how it looks
+                    </StyledCardContent>
+                    <StyledCardActions>
+                        <IconButton aria-label="add to favorites">
+                            <Favorite />
+                        </IconButton>
+                    </StyledCardActions>
                 </StyledCard>
             </StyledHomeContainer>
+            
         </>
     )
 };
